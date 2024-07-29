1a9485cdfcb83d8bb5af7020c7319f432c3dd87b
hahhen <arthurmp2007@hotmail.com>
2024-07-29 19:28:15 -0300

    I'm tired

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => {
       </header>
       <section className="App-hero">
         <div className="App-hero-gradient"></div>
-        <div className="App-hero-wrapper container">
+        <div className="container">
+        <div className="App-hero-wrapper">
           <div className="App-hero-left">
             <h2 className="App-hero-subtitle">
               Rio2016:
@@ -26,21 +27,61 @@ const App = () => {
             <img src={vinandtom} width={300} />
           </div>
         </div>
+        </div>
       </section>
       <section className="App-history">
-        <div className="App-banner">
+        <div className="App-banner" style={{color: "#000"}}>
           <h1>History</h1>
         </div>
-        <div className="container App-history-cont">
-          <div className="App-history-text">
-            <p>Vinicius is the mix of all Brazilian fauna and has the amazing power to stretch himself to Olympic proportions. Tom is the mix of Brazilian fauna and can produce any object from inside his leafy hairdo. Their mission is to spread the Olympic and Paralympic values while breaking all the records of fun.</p>
-            <p>For the Rio2016 Mascots, sports are all about having a great time and they will find a way to transform everything into a sport competition, from cooking dinner to dancing samba.</p>
-            <p>Funny By Nature was the #1 most watched show on Cartoon Network Brazil in 2016 and helped make Vinicius and Tom the greatest record breakers of the Olympic and Paralympic Games, generating triple the licensing revenues of London 2012 and double of Beijing 2008.</p>
-            <p>Created and Produced by Birdo for Cartoon Network.</p>
+        <div className="container">
+          <div className="App-history-cont">
+            <div className="App-history-text">
+              <p>Vinicius is the mix of all Brazilian fauna and has the amazing power to stretch himself to Olympic proportions. Tom is the mix of Brazilian fauna and can produce any object from inside his leafy hairdo. Their mission is to spread the Olympic and Paralympic values while breaking all the records of fun.</p>
+              <p>For the Rio2016 Mascots, sports are all about having a great time and they will find a way to transform everything into a sport competition, from cooking dinner to dancing samba.</p>
+              <p>Funny By Nature was the #1 most watched show on Cartoon Network Brazil in 2016 and helped make Vinicius and Tom the greatest record breakers of the Olympic and Paralympic Games, generating triple the licensing revenues of London 2012 and double of Beijing 2008.</p>
+              <p>Created and Produced by Birdo for Cartoon Network.</p>
+            </div>
+            <div className="App-history-images">
+              <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527541396066-VG60OLSVQP2LJHD6JTWN/vinicius.png?format=1500w" />
+              <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527541608247-Q8AV9B52YQWWBVOKW88N/1_mata_olimpico.jpg?format=750w" />
+              <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527624473756-SIFT7JJWME1H4CPBFFZ9/divertidos_rio.jpg?format=1500w" />
+              <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527624747955-PFKT05Y3R7S38NBOCEUH/divertidos_canoagem2.jpg?format=1500w" />
+              <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527625536462-ZCCD8H0OY4GPTRPULYJN/bg_banner_03.png?format=2500w" />
+              <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527715751920-QJIGH75Z1W3KSSWOARYV/musica.png?format=750w" />
+            </div>
           </div>
-          <div className="App-history-images">
-          <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527541396066-VG60OLSVQP2LJHD6JTWN/vinicius.png?format=1500w"/>
         </div>
+      </section>
+      <section className="App-designs">
+        <div className="App-banner" style={{backgroundImage: "url('https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527625536448-A1QZSQ4JYRX5MCKW6IYJ/bg_banner_02.png?format=2500w')"}}>
+          <h1>Designs</h1>
+        </div>
+        <div className="container">
+          <div className="App-designs-cont">
+            <div className="App-designs-images">
+              <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527602148831-4Y06CL4H8PIK7M0F2ELI/esportes_vinicius_01.jpg?format=1500w" />
+              <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527602149445-PNWRM1WDWQ4ZHT7YGC99/esportes_vinicius_02.jpg?format=1500w" />
+              <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527602150751-QI3RB3V7F9IDPX5BZYZP/esportes_vinicius_03.jpg?format=1500w" />
+              <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527602146748-3ZDBTTYB3IIMD5NJIM20/esportes_tom_01.jpg?format=1500w" />
+              <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527602147429-J2PWLOQJALLJ589HSIN4/esportes_tom_02.jpg?format=1500w" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="App-storybook">
+        <div className="App-banner" style={{backgroundImage: "url('https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527626662933-Z9HF88CDW5341W641TIE/ginasio_05.png?format=2500w')"}}>
+          <h1>Storybook</h1>
+        </div>
+        <div className="container">
+          <div className="App-designs-cont">
+            <div className="App-designs-images">
+              <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527602148831-4Y06CL4H8PIK7M0F2ELI/esportes_vinicius_01.jpg?format=1500w" />
+              <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527602149445-PNWRM1WDWQ4ZHT7YGC99/esportes_vinicius_02.jpg?format=1500w" />
+              <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527602150751-QI3RB3V7F9IDPX5BZYZP/esportes_vinicius_03.jpg?format=1500w" />
+              <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527602146748-3ZDBTTYB3IIMD5NJIM20/esportes_tom_01.jpg?format=1500w" />
+              <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527602147429-J2PWLOQJALLJ589HSIN4/esportes_tom_02.jpg?format=1500w" />
+            </div>
+          </div>
         </div>
       </section>
     </div>
