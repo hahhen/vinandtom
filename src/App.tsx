fb30573ed9d28eb7cd04dca06b16c967ea554763
hahhen <arthurmp2007@hotmail.com>
2024-07-30 21:35:13 -0300

    Typescript update

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,13 @@ const App = () => {
       </header>
       <section className="App-hero">
         <div className="App-hero-gradient"></div>
-        <div className="container">
-          <div className="App-hero-wrapper">
+        <div className="App-container">
+          <div className="App-hero-wrapper flex-col sm:flex-row items-center">
             <div className="App-hero-left">
               <h2 className="App-hero-subtitle">
                 Rio2016:
               </h2>
-              <div className="App-hero-title">
+              <div className="App-hero-title text-5xl md:text-6xl lg:text-8xl">
                 <h1 className="App-hero-title-vin">Vinicius&nbsp;</h1><h1>&&nbsp;</h1><h1 className="App-hero-title-tom">Tom</h1>
               </div>
             </div>
@@ -34,13 +34,13 @@ const App = () => {
         <div className="App-banner" style={{ color: "#000" }}>
           <h1>History</h1>
         </div>
-        <div className="container">
+        <div className="App-container">
           <div className="App-history-cont flex-col md:flex-row">
             <div className="App-history-text">
               <p>Vinicius is the mix of all Brazilian fauna and has the amazing power to stretch himself to Olympic proportions. Tom is the mix of Brazilian fauna and can produce any object from inside his leafy hairdo. Their mission is to spread the Olympic and Paralympic values while breaking all the records of fun.</p>
-              <p>For the Rio2016 Mascots, sports are all about having a great time and they will find a way to transform everything into a sport competition, from cooking dinner to dancing samba.</p>
-              <p>Funny By Nature was the #1 most watched show on Cartoon Network Brazil in 2016 and helped make Vinicius and Tom the greatest record breakers of the Olympic and Paralympic Games, generating triple the licensing revenues of London 2012 and double of Beijing 2008.</p>
-              <p>Created and Produced by Birdo for Cartoon Network.</p>
+              <p className="mt-4">For the Rio2016 Mascots, sports are all about having a great time and they will find a way to transform everything into a sport competition, from cooking dinner to dancing samba.</p>
+              <p className="mt-4">Funny By Nature was the #1 most watched show on Cartoon Network Brazil in 2016 and helped make Vinicius and Tom the greatest record breakers of the Olympic and Paralympic Games, generating triple the licensing revenues of London 2012 and double of Beijing 2008.</p>
+              <p className="mt-4">Created and Produced by Birdo for Cartoon Network.</p>
             </div>
             <div className="App-history-images">
               <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527541396066-VG60OLSVQP2LJHD6JTWN/vinicius.png?format=1500w" />
@@ -57,7 +57,7 @@ const App = () => {
         <div className="App-banner" style={{ backgroundImage: "url('https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527625536448-A1QZSQ4JYRX5MCKW6IYJ/bg_banner_02.png?format=2500w')" }}>
           <h1>Designs</h1>
         </div>
-        <div className="container">
+        <div className="App-container">
           <div className="App-designs-cont">
             <div className="App-designs-images">
               <img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527602148831-4Y06CL4H8PIK7M0F2ELI/esportes_vinicius_01.jpg?format=1500w" />
@@ -73,9 +73,8 @@ const App = () => {
         <div className="App-banner" style={{ backgroundImage: "url('https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527626662933-Z9HF88CDW5341W641TIE/ginasio_05.png?format=2500w')" }}>
           <h1>Storybook</h1>
         </div>
-        <div className="container">
+        <div className="App-container px-8">
           <div className="App-designs-cont">
-            <div className="">
               <Carousel>
                 <CarouselContent>
                   <CarouselItem><img src="https://images.squarespace-cdn.com/content/v1/5aff0e0d3917ee5513af5749/1527712085866-PBNEFV78SHZ2MSZB1WEV/Mascotes+EP+06+Na+cozinha+Storyboard_0007.png?format=1500w" /></CarouselItem>
@@ -86,7 +85,6 @@ const App = () => {
                 <CarouselPrevious />
                 <CarouselNext />
               </Carousel>
-            </div>
           </div>
         </div>
       </section>
